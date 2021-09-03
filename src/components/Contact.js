8c6488f5ddf156e86a02c388e87ee1c396d4539c
Daniel Cope <danielcope@users.noreply.github.com>
2021-09-03 11:54:50 -0700

    added changes to nav

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,19 +8,25 @@ const Contact = () => {
   return (
     <section className="contact" id="contact">
       <section className="contact-links">
-        <a href="https://www.linkedin.com/in/daniel-cope-60853213/">
-          <div className="each-contact">
-            <img src={Email} alt="email" className="contact-img" />
-            <span className="text contact-text">daniel.ash.cope@gmail.com</span>
-          </div>
-        </a>
-        <a href="https://www.linkedin.com/in/daniel-cope-60853213/">
+        <div className="each-contact">
+          <img src={Email} alt="email" className="contact-img" />
+          <span className="text contact-text">daniel.ash.cope@gmail.com</span>
+        </div>
+        <a
+          href="https://www.linkedin.com/in/daniel-cope-60853213/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
           <div className="each-contact">
             <img src={linkedin} alt="linkedin" className="contact-img" />
             <span className="text context-text">linkedin</span>
           </div>
         </a>
-        <a href="https://github.com/danielcope">
+        <a
+          href="https://github.com/danielcope"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
           <div className="each-contact">
             <img src={github} alt="github" className="contact-img" />
             <span className="text contact-text">My GitHub</span>
